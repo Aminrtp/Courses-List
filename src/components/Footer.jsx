268113e2c1ccc339eb6faeaf2ca461818a974299
amin <amindev2025@gmail.com>
2024-12-03 19:51:01 +0330

    make Header and Footer componenet

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        you have successfully passed <strong>33%</strong> of your courses
+    </div>
+  )
+}
+
+export default Footer
